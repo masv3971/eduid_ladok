4d76255abda8df80aa90d835fe5b365865266374
masv3971 <masv@sunet.se>
2021-12-01 15:43:38 +0100

    Add redis mastername.

--- internal/ladok/atom_service.go
+++ internal/ladok/atom_service.go
@@ -37,7 +37,7 @@ func NewAtomService(ctx context.Context, service *Service, channel chan *model.L
 		})
 	default:
 		s.db = redis.NewFailoverClient(&redis.FailoverOptions{
-			MasterName:    "",
+			MasterName:    "redis-cluster",
 			SentinelAddrs: s.Service.config.RedisAddr,
 			DB:            service.config.RedisDB,
 		})
